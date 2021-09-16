0c004f5ae2d88283324ca7097c758fd370f3c955
Lucas Piva <72318923+PIVAO@users.noreply.github.com>
2021-09-16 15:41:46 -0300

    Update Lista Estática AEP.cpp

--- Lista Estática AEP.cpp
+++ Lista Estática AEP.cpp
@@ -2,42 +2,38 @@
 
 void menu() {
 	printf("MENU\n");
-	printf("1 : Cadastrar tanque (em números) \n");
-	printf("2 : Excluir um tanque \n");
-	printf("3 : Mostrar tanques cadastrados \n");
-	printf("4 : Apagar todos os tanques \n");
-	printf("5 : Exibir log de utilização da lista \n");
-	printf("6 : SAIR \n");
+	printf("1 - Cadastrar tanque (em números) \n");
+	printf("2 - Excluir um tanque \n");
+	printf("3 - Mostrar tanques cadastrados\n");
+	printf("4 - Apagar todos os tanques\n");
+	//printf("5 - Exibir log de utilizacao da lista\n");
+	printf("5 - Buscar tanque na lista\n");
+	printf("6 - SAIR \n");
 	printf("Digite a opção desejada: ");
-	scanf("%d",&opcao);
+	scanf("%d", &opcao);
 	system("cls");
 }
 
-void incluir(TANQUE *l){
+void inserir(TANQUE *l){
 	TIPOCHAVE ch;
-	printf("Qual tanque deseja incluir? ");
+	printf("Qual tanque deseja incluir: ");
 	scanf("%d",&ch);
 	REGISTRO reg;
 	reg.chave = ch;
-	if (incluirElemTanqueOrd(reg,l)) {
+	if (inserirElemTanqueOrd(reg,l)) {
 		printf("Tanque %d incluído com sucesso.\n\n",ch);
 	}
-	
 	else {
 		printf("Não foi possível incluir o tanque %d.\n\n",ch);
 	}
 }
 
-/*void buscaUnic() {
-	printf("Digite ")
-}*/
-
 void excluir(TANQUE *l){
 	TIPOCHAVE ch;
-	printf("Tanque a ser excluído: ");
+	printf("Qual tanque deseja excluir: ");
 	scanf("%d",&ch);
 	if (excluirElemTanque(ch,l)) {
-		printf("Tanque %d excluído com sucesso.\n\n",ch);
+    	printf("Tanque %d excluído com sucesso.\n\n",ch);
 	}
 	
 	else {
@@ -47,34 +43,48 @@ void excluir(TANQUE *l){
 
 void imprimir_elementos(TANQUE *l){
 	exibirTanque(l);
-	printf("Número de tanques na lista: %d.\n\n",tam(l));
 }
+
+/*void exibir_detalhes_lista(LISTA *l){
+	printf("Numero de tanques na lista: %d.\n",tamanho(l));
+	printf("Tamanho da lista (em bytes): %d.\n",tamanhoEmBytes(l));
+}*/
 
 void destruir(TANQUE *l){
 	destruirTanque(l);
 	printf("Lista de tanques zerados.\n\n");
 }
 
-void exibir_detalhes_tanque(TANQUE *l){
-	printf("Número de tanques na lista: %d.\n",tam(l));
-	printf("Tamanho da lista de tanques (em bytes): %d.\n\n",tamEmBytes(l));
+void buscar_elemento(TANQUE *l){
+	TIPOCHAVE ch;
+	printf("Qual tanque deseja buscar: ");
+	scanf("%d",&ch);
+	int i = buscaSeq(ch,l);
+	if (i != NUL) {
+		printf("Tanque %i encontrado na posição %i \n\n",ch,i);
+	}
+
+	else {
+		printf("Nao foi possível encontrar tanque %d.\n\n",ch);
+	}
 }
 
 int main(){
+	system("mode 80,25");
 	system("color 0a");
-	system("mode80,25");
 	setlocale (LC_ALL,"portuguese");
 	TANQUE tanque;
 	inicializarTanque(&tanque);
 	while (opcao != 6) {
-	system("cls");
-	menu();
+		system("cls");
+		menu();
 		switch (opcao) {
-		    case 1 : incluir(&tanque); break;
-		    case 2 : excluir(&tanque); break;
-		    case 3 : imprimir_elementos(&tanque); break;
-		    case 4 : destruir(&tanque); break;
-		    case 5 : exibir_detalhes_tanque(&tanque); break;
+			case 1 : inserir(&tanque); break;
+			case 2 : excluir(&tanque); break;
+			case 3 : imprimir_elementos(&tanque); break;
+			case 4 : destruir(&tanque); break;
+			//case 5 : exibir_detalhes_lista(&lista); break;
+			case 5 : buscar_elemento(&tanque); break;
 		}
 	system("pause");
 	}
